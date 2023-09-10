bbf5b9a79af11613453a0cf4cf4907eed296f159
avi-06 <92231892+avi-06@users.noreply.github.com>
2023-09-09 22:45:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 
 st.title("Smhyal")
 st.subheader("Supporting Mental Health via. AI Learning")
+st.markdown("""Supporting Mental Health in Youth via. AI Learning, or Smhyal, is an all-in-one tool for simple self-diagnosis for two of the most common, yet critical, psychological disorders among young adults: anxiety and depression.""")
 
 #sidebar information
 with st.sidebar:
@@ -40,8 +41,8 @@ with st.sidebar:
   - Private residence: Includes those who live independently, dependently (living in a house, apartment, or other similar dwellings and are heavily dependent on others for daily living assistance), and those under the age of 18 years living in a private residence, regardless of living arrangement
   - Other: Those living in a foster home or a care center, residential care facility, crisis residence, institutional care facility, jail and/or correctional facility, or other residential status
   """)
-  
-  st.subheader("Supporting Mental Health in Youth via. AI Learning, or Smhyal, is an all-in-one tool for simple self-diagnosis for two of the most common, yet critical, psychological disorders among youth: anxiety and depression.")
+
+  st.header("Additional Information")
   st.subheader("The Smhyal algorithm is based on a modified version of an artificial neural network (ANN), with an accuracy of more than 94% anxiety and depression diagnosis. The model was trained on the data for young adults from the publicly-available Substance Abuse and Mental Health Service Administration (SAMHSA) MH-CLD survey dataset. The algorithm was created by Avi Verma, under the guidance of Dr. Kaustubh Supekar at Stanford University. We hope that Smyhal will positively impact young adults by counteracting the underdiagnosis of anxiety and depression.")
   st.subheader("None of the data entered in the form or the diagnosis outputs are ever stored or traced.")
   
