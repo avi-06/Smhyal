62a9b56f255da3aff95a5a81e39c5d3dcb6596c8
avi-06 <92231892+avi-06@users.noreply.github.com>
2023-09-09 22:28:44 -0700

    Add sidebar

--- app.py
+++ app.py
@@ -1,43 +1,49 @@
+#general imports
 import streamlit as st
 from tensorflow.keras.models import load_model
 import numpy as np
+
 st.title("Smhyal")
-st.title("Supporting Mental Health via. AI Learning")
-st.subheader("Supporting Mental Health in Youth via. AI Learning, or Smhyal, is an all-in-one tool for simple self-diagnosis for two of the most common, yet critical, psychological disorders among youth: anxiety and depression.")
-st.subheader("The Smhyal algorithm is based on a modified version of an artificial neural network (ANN), with an accuracy of more than 94% anxiety and depression diagnosis. The model was trained on the data for young adults from the publicly-available Substance Abuse and Mental Health Service Administration (SAMHSA) MH-CLD survey dataset. The algorithm was created by Avi Verma, under the guidance of Dr. Kaustubh Supekar at Stanford University. We hope that Smyhal will positively impact young adults by counteracting the underdiagnosis of anxiety and depression.")
-st.subheader("None of the data entered in the form or the diagnosis outputs are ever stored or traced.")
-st.header("Additional FAQ")
-st.markdown("""
-Education:
-- If you are under 18 years of age: current grade level for school (if attended in the past three months) or highest level ever attended (if not attended within the past three months
-- If you are at or above 18 years of age: Highest level of educational attended, whether currently in school or not""")
-st.markdown("""
-Race:
-- American Indian/Alaska Native: Origins in any of the original people of North America and South America (including Central America) and who maintain tribal affiliation or community attachment
-- Asian: Origins in Far East, Indian subcontinent, or Southeast Asia; primarily includes Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand, and Vietnam
-- Black or African American:  Origins in any of the Black racial groups of Africa
-- Native Hawaiian or Other Pacific Islander: Origins in any of the original people of Hawaii, Guam, Samoa, or other Pacific Islands
-- White: A person having origins in any of the original people of Europe, the Middle East, or North Africa""")
-st.markdown("""
-Ethnicity:
-- Mexican: Of Mexican origin, regardless of race
-- Puerto Rican: Of Puerto Rican origin, regardless of race
-- Other Hispanic or Latino origin: Of known Central or South American or any other Spanish cultural origin (including Spain), other than Puerto Rican or Mexican, regardless of race""")
-st.markdown("""
-Martial Status:
-- Never married: If you are single or whose only marriage was annulled
-- Now married: If you are married, living together as married, living with partners, or cohabitating
-- Separated: If you are legally separated or otherwise absent from your spouse because of marital discord""")
-st.markdown("""
-Employment Status:
-- Full-time: Includes those in full-time employment
-- Not in the labor force: Not employed and not actively looking for work during the past 30 days (i.e. you are not interested in work or you have been discouraged to look for work)""")
-st.markdown("""
-Residential Status
-- Homeless: Those who have no fixed address, including homeless shelters
-- Private residence: Includes those who live independently, dependently (living in a house, apartment, or other similar dwellings and are heavily dependent on others for daily living assistance), and those under the age of 18 years living in a private residence, regardless of living arrangement
-- Other: Those living in a foster home or a care center, residential care facility, crisis residence, institutional care facility, jail and/or correctional facility, or other residential status
-""")
+
+#sidebar information
+with st.sidebar:
+  st.title("Supporting Mental Health via. AI Learning")
+  st.subheader("Supporting Mental Health in Youth via. AI Learning, or Smhyal, is an all-in-one tool for simple self-diagnosis for two of the most common, yet critical, psychological disorders among youth: anxiety and depression.")
+  st.subheader("The Smhyal algorithm is based on a modified version of an artificial neural network (ANN), with an accuracy of more than 94% anxiety and depression diagnosis. The model was trained on the data for young adults from the publicly-available Substance Abuse and Mental Health Service Administration (SAMHSA) MH-CLD survey dataset. The algorithm was created by Avi Verma, under the guidance of Dr. Kaustubh Supekar at Stanford University. We hope that Smyhal will positively impact young adults by counteracting the underdiagnosis of anxiety and depression.")
+  st.subheader("None of the data entered in the form or the diagnosis outputs are ever stored or traced.")
+  st.header("Additional FAQ")
+  st.markdown("""
+  Education:
+  - If you are under 18 years of age: current grade level for school (if attended in the past three months) or highest level ever attended (if not attended within the past three months
+  - If you are at or above 18 years of age: Highest level of educational attended, whether currently in school or not""")
+  st.markdown("""
+  Race:
+  - American Indian/Alaska Native: Origins in any of the original people of North America and South America (including Central America) and who maintain tribal affiliation or community attachment
+  - Asian: Origins in Far East, Indian subcontinent, or Southeast Asia; primarily includes Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand, and Vietnam
+  - Black or African American:  Origins in any of the Black racial groups of Africa
+  - Native Hawaiian or Other Pacific Islander: Origins in any of the original people of Hawaii, Guam, Samoa, or other Pacific Islands
+  - White: A person having origins in any of the original people of Europe, the Middle East, or North Africa""")
+  st.markdown("""
+  Ethnicity:
+  - Mexican: Of Mexican origin, regardless of race
+  - Puerto Rican: Of Puerto Rican origin, regardless of race
+  - Other Hispanic or Latino origin: Of known Central or South American or any other Spanish cultural origin (including Spain), other than Puerto Rican or Mexican, regardless of race""")
+  st.markdown("""
+  Martial Status:
+  - Never married: If you are single or whose only marriage was annulled
+  - Now married: If you are married, living together as married, living with partners, or cohabitating
+  - Separated: If you are legally separated or otherwise absent from your spouse because of marital discord""")
+  st.markdown("""
+  Employment Status:
+  - Full-time: Includes those in full-time employment
+  - Not in the labor force: Not employed and not actively looking for work during the past 30 days (i.e. you are not interested in work or you have been discouraged to look for work)""")
+  st.markdown("""
+  Residential Status
+  - Homeless: Those who have no fixed address, including homeless shelters
+  - Private residence: Includes those who live independently, dependently (living in a house, apartment, or other similar dwellings and are heavily dependent on others for daily living assistance), and those under the age of 18 years living in a private residence, regardless of living arrangement
+  - Other: Those living in a foster home or a care center, residential care facility, crisis residence, institutional care facility, jail and/or correctional facility, or other residential status
+  """)
+  
 #importing models
 mod1 = load_model('depression.h5')
 mod2 = load_model('anxiety.h5')
