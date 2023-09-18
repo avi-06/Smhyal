6deb3728583a31aa8f42a3b1089667a615e9ab8d
Avi Verma <92231892+avi-06@users.noreply.github.com>
2023-09-17 20:41:14 -0700

    Increase image size temporarily

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ with st.sidebar:
                     background-image: url(https://drive.google.com/uc?export=view&id=19bGh52gQKRQt_8G_3k5j1mW8FqUB5Plm);
                     background-repeat: no-repeat;
                     padding-top: 80px;
-                    background-position: 40px 40px;
+                    background-position: 100px 100px;
                 }}
             </style>
             """,
