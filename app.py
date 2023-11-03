3cd5e4bc4c76da3c65e3ed4d51f942e5d8df36db
Avi Verma <92231892+avi-06@users.noreply.github.com>
2023-11-02 19:30:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,32 +4,6 @@ from tensorflow.keras.models import load_model
 from streamlit_js_eval import streamlit_js_eval
 import numpy as np
 
-hide_streamlit_style = """
-                <style>
-                div[data-testid="stToolbar"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                div[data-testid="stStatusWidget"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
-                header {
-                visibility: hidden;
-                height: 0%;
-                }
-                footer {
-                visibility: hidden;
-                height: 0%;
-                }
-                </style>
-                """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.title("Smhyal")
